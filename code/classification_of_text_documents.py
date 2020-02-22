cb2cac6187f18d1e392b8f3a0801c31b5dee0e42
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2020-02-22 16:05:00 -0500

    Plotting training and test time

--- code/classification_of_text_documents.py
+++ code/classification_of_text_documents.py
@@ -326,8 +326,8 @@ if opts.filtered:
 else:
     plt.title("Accuracy score for the 20 news group dataset")
 plt.barh(indices, score, .2, label="score", color='navy')
-# plt.barh(indices + .3, training_time, .2, label="training time", color='c')
-# plt.barh(indices + .6, test_time, .2, label="test time", color='darkorange')
+plt.barh(indices + .3, training_time, .2, label="training time (in seconds)", color='c')
+plt.barh(indices + .6, test_time, .2, label="test time (in seconds)", color='darkorange')
 plt.yticks(())
 plt.legend(loc='best')
 plt.subplots_adjust(left=.25)
