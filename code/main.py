6787a56ad41353a6c547169dd192cd0a39eb30ad
Ramon Figueiredo Pessoa <rpessoa@accedian.com>
2020-03-04 17:02:59 -0500

    Code refactoring

--- code/main.py
+++ code/main.py
@@ -24,11 +24,11 @@ from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.model_selection import cross_validate
 from sklearn.utils.extmath import density
 
-from datasets.load_dataset import load_twenty_news_groups, load_imdb_reviews
+from argument_parser.argument_parser import get_options, show_option
+from datasets.load_dataset import load_dataset
 from model_selection.ml_algorithm_pair_list import get_ml_algorithm_pair_list
 from utils.dataset_enum import Dataset
 from utils.ml_classifiers_enum import Classifier
-from argument_parser.argument_parser import get_options, show_option
 
 
 def pre_process_options():
@@ -44,97 +44,8 @@ def pre_process_options():
                             datefmt='%m/%d/%Y %I:%M:%S %p')
     dataset = options.dataset
     dataset = dataset.upper().strip()
-    shuffle = (not options.not_shuffle_dataset)
 
-    return options, dataset, shuffle
-
-
-def load_dataset(dataset):
-    if dataset == Dataset.TWENTY_NEWS_GROUPS.name:
-
-        X_test, X_train, data_train, y_test, y_train = load_twenty_news_group_dataset()
-
-    elif dataset == Dataset.IMDB_REVIEWS.name:
-
-        X_test, X_train, y_test, y_train = load_imdb_reviews_dataset()
-    else:
-        logging.error("Loading dataset: Wrong dataset name = '{}'. Expecting: {} OR {}".format(dataset,
-                                                                                               Dataset.TWENTY_NEWS_GROUPS.name,
-                                                                                               Dataset.IMDB_REVIEWS.name))
-        exit(0)
-
-    print('data loaded')
-
-    if dataset == Dataset.TWENTY_NEWS_GROUPS.name:
-        # order of labels in `target_names` can be different from `categories`
-        target_names = data_train.target_names
-    else:
-        # IMDB_REVIEWS dataset
-        # If binary classification: 0 = neg and 1 = pos.
-        # If multi-class classification use the review scores: 1, 2, 3, 4, 7, 8, 9, 10
-        if options.use_imdb_binary_labels:
-            target_names = ['0', '1']
-        else:
-            target_names = ['1', '2', '3', '4', '7', '8', '9', '10']
-
-    def size_mb(docs):
-        return sum(len(s.encode('utf-8')) for s in docs) / 1e6
-
-    data_train_size_mb = size_mb(X_train)
-    data_test_size_mb = size_mb(X_test)
-
-    print("%d documents - %0.3fMB (training set)" % (
-        len(X_train), data_train_size_mb))
-
-    print("%d documents - %0.3fMB (test set)" % (
-        len(X_test), data_test_size_mb))
-
-    if dataset == Dataset.TWENTY_NEWS_GROUPS.name:
-        print("%d categories" % len(target_names))
-    print()
-
-    return X_train, y_train, X_test, y_test, target_names, data_train_size_mb, data_test_size_mb
-
-
-def load_imdb_reviews_dataset():
-    print("Loading {} dataset:".format(Dataset.IMDB_REVIEWS.name))
-    X_train, y_train = load_imdb_reviews(subset='train', binary_labels=options.use_imdb_binary_labels,
-                                         verbose=options.show_imdb_reviews, shuffle=shuffle,
-                                         random_state=options.random_state)
-    X_test, y_test = load_imdb_reviews(subset='test', binary_labels=options.use_imdb_binary_labels,
-                                       verbose=options.show_imdb_reviews, shuffle=shuffle,
-                                       random_state=options.random_state)
-    return X_test, X_train, y_test, y_train
-
-
-def load_twenty_news_group_dataset():
-    if options.twenty_news_using_four_categories:
-        categories = [
-            'alt.atheism',
-            'talk.religion.misc',
-            'comp.graphics',
-            'sci.space',
-        ]
-    else:
-        categories = None
-
-    if options.twenty_news_with_no_filter:
-        remove = ()
-    else:
-        remove = ('headers', 'footers', 'quotes')
-
-    print("Loading {} dataset for categories:".format(Dataset.TWENTY_NEWS_GROUPS.name))
-
-    data_train = load_twenty_news_groups(subset='train', categories=categories, shuffle=shuffle, random_state=0,
-                                         remove=remove)
-
-    data_test = load_twenty_news_groups(subset='test', categories=categories, shuffle=shuffle,
-                                        random_state=options.random_state,
-                                        remove=remove)
-    X_train, y_train = data_train.data, data_train.target
-    X_test, y_test = data_test.data, data_test.target
-
-    return X_test, X_train, data_train, y_test, y_train
+    return options, dataset
 
 
 def extracting_features(X_train, X_test):
@@ -504,7 +415,7 @@ if __name__ == '__main__':
 
     options, parser = get_options()
 
-    options, dataset_option, shuffle = pre_process_options()
+    options, dataset_option = pre_process_options()
 
     show_option(options, parser)
 
@@ -524,7 +435,7 @@ if __name__ == '__main__':
 
     for dataset in dataset_list:
 
-        X_train, y_train, X_test, y_test, target_names, data_train_size_mb, data_test_size_mb = load_dataset(dataset)
+        X_train, y_train, X_test, y_test, target_names, data_train_size_mb, data_test_size_mb = load_dataset(dataset, options)
 
         vectorizer, X_train, X_test = extracting_features(X_train, X_test)
 
