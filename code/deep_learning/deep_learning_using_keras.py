77e17971691511eb42f050129ed58662a56e375d
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2020-03-11 23:53:32 -0400

    Code refactoring

--- code/deep_learning/deep_learning_using_keras.py
+++ code/deep_learning/deep_learning_using_keras.py
@@ -16,7 +16,6 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from datasets.load_dataset import load_dataset
 from feature_extraction.nltk_features_extraction import apply_nltk_feature_extraction
 from feature_extraction.vectorizer import extract_text_features
-from feature_selection.select_k_best import select_k_best_using_chi2
 from metrics.ml_metrics import print_classification_report, print_ml_metrics, print_confusion_matrix
 from plotting.plot import plot_history
 from utils.dataset_enum import Dataset
@@ -53,7 +52,7 @@ def get_dataset_list(options):
     return dataset_list
 
 
-def one_hot_enconder_in_multi_class_dataset(dataset, options, y_test, y_train):
+def one_hot_enconder(dataset, options, y_test, y_train):
     if (dataset == Dataset.IMDB_REVIEWS.name and options.use_imdb_multi_class_labels) \
             or dataset == Dataset.TWENTY_NEWS_GROUPS.name:
         le_train = LabelEncoder()
@@ -73,21 +72,18 @@ def one_hot_enconder_in_multi_class_dataset(dataset, options, y_test, y_train):
     return y_test, y_train
 
 
-def print_results(X_test, algorithm_name, dataset, model, test_time, training_accuracy, training_loss, training_time, y_test):
+def print_results(dataset, algorithm_name, training_loss, training_accuracy, test_loss, test_accuracy, training_time, test_time):
     print("\tDataset: {}".format(dataset))
     print("\tAlgorithm: {}".format(algorithm_name))
     print("\tTraining loss: {:.4f}".format(training_loss))
     print("\tTraining accuracy score: {:.2f}%".format(training_accuracy * 100))
-    test_loss, test_accuracy = model.evaluate(X_test, y_test, verbose=False)
     print("\tTest loss: {:.4f}".format(test_loss))
     print("\tTest accuracy score: {:.2f}%".format(test_accuracy * 100))
     print("\tTraining time: {:.4f}".format(training_time))
     print("\tTest time: {:.4f}".format(test_time))
-    return test_accuracy
 
 
 def run_deep_learning_KerasDL1(options):
-
     manage_logs(options)
 
     logging.info("Program started...")
@@ -99,18 +95,9 @@ def run_deep_learning_KerasDL1(options):
 
         X_train, y_train, X_test, y_test, target_names, data_train_size_mb, data_test_size_mb = load_dataset(dataset, options)
 
-        y_test, y_train = one_hot_enconder_in_multi_class_dataset(dataset, options, y_test, y_train)
+        y_test, y_train = one_hot_enconder(dataset, options, y_test, y_train)
 
-        vectorizer, X_train, X_test = extract_text_features(X_train, X_test, options, data_train_size_mb,
-                                                            data_test_size_mb)
-
-        if options.use_hashing:
-            feature_names = None
-        else:
-            feature_names = vectorizer.get_feature_names()
-
-        if options.chi2_select:
-            select_k_best_using_chi2(X_train, y_train, X_test, feature_names, options)
+        vectorizer, X_train, X_test = extract_text_features(X_train, X_test, options, data_train_size_mb, data_test_size_mb)
 
         input_dim = X_train.shape[1]  # Number of features
 
@@ -134,7 +121,7 @@ def run_deep_learning_KerasDL1(options):
                 epochs = 12
             elif dataset == Dataset.IMDB_REVIEWS.name and options.use_imdb_multi_class_labels:
                 epochs = 3
-            else: # IMDB_REVIEWS with binary classification
+            else:  # IMDB_REVIEWS using binary classification
                 epochs = 1
 
         if not options.epochs:
@@ -144,16 +131,18 @@ def run_deep_learning_KerasDL1(options):
             print('\n\nNUMBER OF EPOCHS USED: {}\n'.format(options.epochs))
             history = model.fit(X_train, y_train, epochs=options.epochs, verbose=False, validation_data=(X_test, y_test), batch_size=10)
 
-
         training_time = time() - start
 
-        start = time()
+
         training_loss, training_accuracy = model.evaluate(X_train, y_train, verbose=False)
+
+        start = time()
+        test_loss, test_accuracy = model.evaluate(X_test, y_test, verbose=False)
         test_time = time() - start
 
         algorithm_name = "Deep Learning using Keras 1 (KERAS_DL1)"
 
-        test_accuracy = print_results(X_test, algorithm_name, dataset, model, test_time, training_accuracy, training_loss, training_time, y_test)
+        print_results(dataset, algorithm_name, training_loss, training_accuracy, test_loss, test_accuracy, training_time, test_time)
 
         plt.style.use('ggplot')
 
@@ -174,7 +163,6 @@ IMDB Review - Deep Model
 Reference: https://www.kaggle.com/nilanml/imdb-review-deep-model-94-89-accuracy
 '''
 def run_deep_learning_KerasDL2(options):
-
     logging.info("Program started...")
 
     dataset_list = get_dataset_list(options)
@@ -187,7 +175,7 @@ def run_deep_learning_KerasDL2(options):
         X_train = apply_nltk_feature_extraction(X_train, options, label='X_train')
         X_test = apply_nltk_feature_extraction(X_test, options, label='X_test')
 
-        y_test, y_train = one_hot_enconder_in_multi_class_dataset(dataset, options, y_test, y_train)
+        y_test, y_train = one_hot_enconder(dataset, options, y_test, y_train)
 
         print('\t===> Tokenizer: fit_on_texts(X_train)')
         max_features = 6000
@@ -219,7 +207,7 @@ def run_deep_learning_KerasDL2(options):
                 epochs = 4
             elif dataset == Dataset.IMDB_REVIEWS.name and options.use_imdb_multi_class_labels:
                 epochs = 1
-            else: # IMDB_REVIEWS with binary classification
+            else:  # IMDB_REVIEWS using binary classification
                 epochs = 4
 
         if not options.epochs:
@@ -262,14 +250,15 @@ def run_deep_learning_KerasDL2(options):
 
         print_confusion_matrix(options, y_pred, y_test)
 
-        start = time()
         training_loss, training_accuracy = model.evaluate(X_t, y, verbose=False)
+
+        start = time()
+        test_loss, test_accuracy = model.evaluate(X_te, y_test, verbose=False)
         test_time = time() - start
+
         algorithm_name = "Deep Learning using Keras 2 (KERAS_DL2)"
 
-        test_accuracy = print_results(X_te, algorithm_name, dataset, model, test_time, training_accuracy, training_loss, training_time, y_test)
-        test_accuracy = print_results(X_te, algorithm_name, dataset, model, test_time, training_accuracy, training_loss, training_time, y_test)
-
+        print_results(dataset, algorithm_name, training_loss, training_accuracy, test_loss, test_accuracy, training_time, test_time)
         plt.style.use('ggplot')
 
         if dataset == Dataset.TWENTY_NEWS_GROUPS.name:
